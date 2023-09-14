e235810a9b225fc068ee1e06964837001b943a60
robers051 <145029884+robers051@users.noreply.github.com>
2023-09-14 15:40:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New health Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.textr('Hard-Boiled Free-Range Egg')
